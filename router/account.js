772b59f61c85e2ebdefb54f43f93b4e1cf1bf3e7
kelly0924 <hyj09240@gmail.com>
2023-04-18 07:34:08 +0000

    CREATE PUT ACCOUNT API PUT acount api 사용자 정보수정하는 api.

--- router/account.js
+++ router/account.js
@@ -214,5 +214,78 @@ router.get("/",async(req,res)=>{
 
 })
 
+// my_page 수정
+
+router.put("/",async(req,res)=>{
+    // Request Data
+    const refreshTokenValue = req.headers.refresh_token
+    const accessTokenValue = req.headers.access_token
+    const emailValue = req.body.email
+    const nameValue = req.body.name 
+    
+    //Respons Data
+    
+    const result = {
+        "success": false,
+        "message": null,
+        "access_token": null,
+        "refresh_token": null
+    }
+    
+    try{
+        if (emailValue === null || emailValue === undefined || emailValue === "") {
+            throw new Error("이메일 값이 올바르지 않습니다.")
+        } else if(nameValue === null || nameValue === undefined || nameValue === "") {
+            throw new Error("이름 값이 올바르지 않습니다.")
+        }else{
+       
+            if(accessTokenValue !== undefined || refreshTokenValue !== undefined){ 
+
+                const accountIndexValue = accessVerify(accessTokenValue).payload
+
+                if(accessVerify(accessTokenValue).success === true){//treu일 경우-> access_token이 유효한 경우 
+                    
+                    if(refreshVerify(refreshTokenValue).message === "token expired"){
+                        const temp = await updateRefreshToken(accountIndexValue)
+                        console.log("여기동",temp)
+                        res.send(temp)
+                    }else{
+                    
+                        const connection = await db.getConnection()
+                        const sql = `
+                            UPDATE account SET user_name = ?,emailFROM =? account WHERE account_index = ?
+                        `
+                        const values = [nameValue, emailValue, accountIndexValue]
+                        await connection.query(sql, values)
+                        result.success = true
+
+                        connection.release()
+                        res.send(result)
+                    }
+                
+                }else if(accessVerify(accessTokenValue).message === "token expired"){//access_token이 완료된 경우 
+                    //refresh_token이 유효한 경우 
+                    if(refreshVerify(refreshTokenValue)){//true 인 경우 -> refresh_token이 유효한 경우 새로운 access_token생성
+                        const accountIndexValue = refreshVerify(refreshTokenValue).payload
+                        const temp = await newAccessToken(accountIndexValue, refreshTokenValue)
+                        res.send(temp)
+
+                    }else{
+                        throw new Error("모든 토큰이 완료 되었습니다.")
+                    }
+
+                }
+            }else{
+                throw new Error("올바르지 않은 토큰입니다.")
+            }
+        }
+    }catch(e){
+        result.message = e.message
+        console.log("PUT /account API ERR : ", e.message)
+    }
+
+    
+
+})
 
 module.exports = router
