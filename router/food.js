33e1fd8483e826f80af42597c626cf85962f7989
kelly0924 <hyj09240@gmail.com>
2023-04-21 06:42:50 +0000

    CREATE GET FOOD/SOLUTION API GET food/solution api 는 탄소배출량이 적은 음식부터 순서대로 가져오는 api.

--- router/food.js
+++ router/food.js
@@ -89,11 +89,81 @@ router.get("/",async(req,res)=>{
         }
     }catch(e){
         result.message = e.message
-        console.log("GET /account API ERR : ", e.message)
+        console.log("GET /food API ERR : ", e.message)
     }
 
     
 
 })
+
+//food 솔류션 가져오기_ carbon 배출량이 적은 것 부터 나열해주기
+
+router.get("/solution",async(req,res)=>{
+    // Request Data
+    const refreshTokenValue = req.headers.refresh_token
+    const accessTokenValue = req.headers.access_token
+
+    //Respons Data
+    
+    const result = {
+        "success": false,
+        "message": null,
+        "access_token": null,
+        "refresh_token": null,
+        "data": 0
+    }
+    
+    try{
+        
+    
+        if(accessTokenValue !== undefined || refreshTokenValue !== undefined){ 
+
+            const accountIndexValue = accessVerify(accessTokenValue).payload
+            if(accessVerify(accessTokenValue).success === true){//treu일 경우-> access_token이 유효한 경우 
+                
+                if(refreshVerify(refreshTokenValue).message === "token expired"){
+                    const temp = await updateRefreshToken(accountIndexValue)
+                    console.log("여기동",temp)
+                    res.send(temp)
+                }else{
+                   
+                    const connection = await db.getConnection()
+                    const sql = `
+                        SELECT menu, carbon  FROM food ORDER BY carbon ASC
+                    `
+                    const [rows]  = await connection.query(sql)
+                    result.success = true
+                    result.data = rows
+    
+                    await connection.release()
+                    res.send(result)
+                }
+            
+            }else if(accessVerify(accessTokenValue).message === "token expired"){//access_token이 완료된 경우 
+                //refresh_token이 유효한 경우 
+                if(refreshVerify(refreshTokenValue)){//true 인 경우 -> refresh_token이 유효한 경우 새로운 access_token생성
+                    const accountIndexValue = refreshVerify(refreshTokenValue).payload
+                    const temp = await newAccessToken(accountIndexValue, refreshTokenValue)
+                    res.send(temp)
+
+                }else{
+                    throw new Error("모든 토큰이 완료 되었습니다.")
+                }
+
+            }else{
+                throw new Error("올바르지 않은 토큰입니다.")
+            }
+        }else{
+            throw new Error("올바르지 않은 토큰입니다.")
+        }
+    }catch(e){
+        result.message = e.message
+        console.log("GET /food/solution API ERR : ", e.message)
+    }
+
+    
+
+})
+
 
 module.exports = router
